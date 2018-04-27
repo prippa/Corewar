885ccca8eb2e873525728a07885e6366df22609b
prippa <pavelrippa@gmail.com>
2018-04-27 18:52:03 +0300

    Viktor printf power (in test.c)

--- vm/source/test.c
+++ vm/source/test.c
@@ -2,7 +2,18 @@
 
 int main(void)
 {
-    ft_printf("HI!\n");
-    ft_putstr("!@#\n");
+	srand(time(NULL));
+
+	int i = 0;
+	int main = 0;
+	int back = 0;
+
+	while (i < 100)
+	{
+		ft_printf("%[*]{*}s\n", main, back, "Hello world");
+		main = (rand() % 2147483647 + 1);
+		back = (rand() % 2147483647 + 1);
+		i++;
+	}
     return (0);
 }
