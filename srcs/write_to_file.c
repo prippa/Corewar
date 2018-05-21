aef36244af0598e52809048bc713d6dce691be03
contrbas@gmail.com <K4rmU78B>
2018-05-21 19:31:37 +0300

    ALL

--- srcs/write_to_file.c
+++ srcs/write_to_file.c
@@ -13,6 +13,11 @@
 #include "../libft/ft_printf.h"
 #include <fcntl.h>
 
+typedef struct s_toto
+{
+	char *str;
+}				t_toto;
+
 int		ft_string_size(char *str)
 {
 	int i;
@@ -44,12 +49,37 @@ void	create_name_string(char *file_name_to_open, char *data_to_be_written)
 	free(res);
 }
 
+void	create_name_struct(char *file_name_to_open, t_toto sample)
+{
+	int len; 	// len till '.';
+	int fd; 	// file descriptor;
+	char *tmp; 	// buf;
+	char *res; 	// string after join;
+
+	len = ft_string_size(file_name_to_open);
+	tmp = ft_strnew(len);
+	ft_strncpy(tmp, file_name_to_open, len);
+	res = ft_strjoin(tmp, ".cor");
+	free(tmp);
+
+	fd = open(res, O_WRONLY | O_CREAT, 0777);
+
+	write(fd, &sample, 5); // can be int value;
+
+	close(fd);
+	free(res);
+}
+
 
 int		main(int argc, char **argv)
 {
 	(void)argc;	
 
-	create_name_string(argv[1], "12345");
+	t_toto sample;
+
+	sample.str = ft_strdup("12345");
+
+	create_name_struct(argv[1], sample);
 
 	system("leaks -q corewar");
 }
