cab918da2f2d900fce57ba4bb21bc03fd2805d04
contrbas@gmail.com <K4rmU78B>
2018-05-24 18:03:11 +0300

    ALL

--- srcs/write_to_file.c
+++ srcs/write_to_file.c
@@ -139,69 +139,6 @@ void	init(char *str)
 // 	free(str);
 // 	return (dec_val);
 // }
-
-int		len_to_copy(char *str)
-{
-	int i;
-	int len;
-
-	i = 0;
-	len = ft_strlen(str) - 1;
-	while (str[len] && str[len] != '.' && str[len] != '/')
-	{
-		i++;
-		len--;
-	}
-	// ft_printf("i -> %d\n", i);
-	// ft_printf("len -> %d\n", ft_string_size(str));
-	if (ft_string_size(str) == i)
-		return (0);
-	return (i + 1);
-}
-
-void	write_to_struct(char *file_name, t_toto *sample)
-{
-	int		len;
-	int		fd;
-	int		copy_start;
-	char	*tmp;
-	char	*res;
-	void	*ar;
-	unsigned int 	asd;
-
-	asd = 1792; // inverted value = 1792;
-
-	// asd = hexadecimal_to_decimal(dec_to_hexa(asd));
-	// ft_printf("%u\n", asd);
-	// short buf = asd;
-	ar = &asd;
-	// short x = *(short *)ar;
-	// ft_printf("%u\n",x);
-	// int -> short | char'
-	len = ft_string_size(file_name);
-	copy_start = ft_strlen(file_name) - len;
-	// ft_printf("copy_start -> %d\n", copy_start);
-	tmp = ft_strnew(len - len_to_copy(file_name));
-	// ft_printf("%s\n", "Check");
-	ft_strncpy(tmp, &file_name[copy_start], len - len_to_copy(file_name));
-	res = ft_strjoin(tmp, ".cor");
-	free(tmp);
-	fd = open(res, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	// sample->magic = hexadecimal_to_decimal(dec_to_hexa(sample->magic));
-	// sample->prog_size = hexadecimal_to_decimal(dec_to_hexa(sample->prog_size));
-	ft_putnbr(sample->prog_size);
-	// ft_printf("unsigned int ->%u\n", sample->magic);
-	// ft_printf("str size -> %d\n", sizeof(sample->prog_name));
-	// ft_printf("unsigned int size -> %d\n", sizeof(sample->magic));
-	write(fd, sample, sizeof(sample->magic) + sizeof(sample->prog_name)
-			+ sizeof(sample->prog_size) + sizeof(sample->comment));
-
-	write(fd, (short*)ar, 2);
-
-	close(fd);
-	free(res);
-}
-
 void	reverse_binary_string(char *str)
 {
 	ft_printf("initial string -> %s\n", str);
@@ -222,37 +159,29 @@ void	reverse_binary_string(char *str)
 	ft_printf("buf2 -> %s\n", buf);
 
 	ft_strncpy(&str[8], &str[16], 8);
-	ft_strcpy(&str[24], buf);
+	ft_strncpy(&str[16], buf, 8);
+
 	ft_printf("2. swap -> %s\n", str);
 }
 
-// C++ program to convert binary to decimal
-// #include<iostream>
-// using namespace std;
- 
-// Function to convert binary to decimal
-// unsigned int binaryToDecimal(char *str)
-// {
-//     int num = n;
-//     int dec_value = 0;
-     
-//     // Initializing base value to 1, i.e 2^0
-//     int base = 1;
-     
-//     int temp = num;
-//     while (temp)
-//     {
-//         int last_digit = temp % 10;
-//         temp = temp/10;
-         
-//         dec_value += last_digit*base;
-         
-//         base = base*2;
-//     }
-     
-//     return dec_value;
-// }
+int		len_to_copy(char *str)
+{
+	int i;
+	int len;
 
+	i = 0;
+	len = ft_strlen(str) - 1;
+	while (str[len] && str[len] != '.' && str[len] != '/')
+	{
+		i++;
+		len--;
+	}
+	// ft_printf("i -> %d\n", i);
+	// ft_printf("len -> %d\n", ft_string_size(str));
+	if (ft_string_size(str) == i)
+		return (0);
+	return (i + 1);
+}
 int bstr_to_dec(char * str)
 {
     int val = 0;
@@ -287,6 +216,81 @@ char	*ft_to_binary(unsigned int n)
 	return (reverse);
 }
 
+void	write_to_struct(char *file_name, t_toto *sample)
+{
+	int		len;
+	int		fd;
+	int		copy_start;
+	char	*tmp;
+	char	*res;
+	void	*ar;
+	unsigned int 	asd;
+
+	asd = 1792; // inverted value = 1792;
+
+	// asd = hexadecimal_to_decimal(dec_to_hexa(asd));
+	// ft_printf("%u\n", asd);
+	// short buf = asd;
+	ar = &asd;
+	// short x = *(short *)ar;
+	// ft_printf("%u\n",x);
+	// int -> short | char'
+	len = ft_string_size(file_name);
+	copy_start = ft_strlen(file_name) - len;
+	// ft_printf("copy_start -> %d\n", copy_start);
+	tmp = ft_strnew(len - len_to_copy(file_name));
+	// ft_printf("%s\n", "Check");
+	ft_strncpy(tmp, &file_name[copy_start], len - len_to_copy(file_name));
+	res = ft_strjoin(tmp, ".cor");
+	free(tmp);
+	fd = open(res, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	// sample->prog_size = hexadecimal_to_decimal(dec_to_hexa(sample->prog_size));
+	// ft_putnbr(sample->prog_size);
+	sample->prog_size = bstr_to_dec(ft_to_binary(23));
+	sample->magic = bstr_to_dec(ft_to_binary(15369203));;
+	// sample->magic = 385875968;
+
+	// ft_printf("unsigned int ->%u\n", sample->magic);
+	// ft_printf("str size -> %d\n", sizeof(sample->prog_name));
+	// ft_printf("unsigned int size -> %d\n", sizeof(sample->magic));
+	write(fd, sample, sizeof(sample->magic) + sizeof(sample->prog_name)
+			+ sizeof(sample->prog_size) + sizeof(sample->comment));
+
+	// write(fd, (short*)ar, 2);
+
+	close(fd);
+	free(res);
+}
+
+
+// C++ program to convert binary to decimal
+// #include<iostream>
+// using namespace std;
+ 
+// Function to convert binary to decimal
+// unsigned int binaryToDecimal(char *str)
+// {
+//     int num = n;
+//     int dec_value = 0;
+     
+//     // Initializing base value to 1, i.e 2^0
+//     int base = 1;
+     
+//     int temp = num;
+//     while (temp)
+//     {
+//         int last_digit = temp % 10;
+//         temp = temp/10;
+         
+//         dec_value += last_digit*base;
+         
+//         base = base*2;
+//     }
+     
+//     return dec_value;
+// }
+
+
 int		main(int argc, char **argv)
 {
 	t_toto	sample;
@@ -296,7 +300,6 @@ int		main(int argc, char **argv)
 	ft_strcpy(sample.prog_name, "zork");
 	ft_bzero(sample.comment, sizeof(sample.comment));
 	ft_strcpy(sample.comment, "just a basic living prog");
-	sample.magic = 15369203;
 
 	// unsigned int test;
 	// unsigned int x = 23;
@@ -304,7 +307,6 @@ int		main(int argc, char **argv)
 
 	// ft_printf("-----------%u ->", bstr_to_dec(ft_to_binary(23)));
 
-	sample.prog_size = bstr_to_dec(ft_to_binary(23));
 	
 	write_to_struct(argv[1], &sample);
 
