e7f6bec1a43525f9ab03c15ac2bef37c009b55af
contrbas@gmail.com <K4rmU78B>
2018-05-25 17:59:59 +0300

    ALL

--- assembler/srcs/write_to_struct.c
+++ assembler/srcs/write_to_struct.c
@@ -18,6 +18,11 @@ void	initial(char *argv_name, t_asm *am)
 	char	*file_name;
 	char	*test;
 
+	ft_printf("argv -> %s\n", argv_name);
+
+
+
+
 	file_name = generate_file_name(argv_name);
 	fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	test = ft_to_binary(am->hd.prog_size);
@@ -26,6 +31,7 @@ void	initial(char *argv_name, t_asm *am)
 	test = ft_to_binary(15369203);
 	am->hd.magic = bstr_to_dec(test);
 	free(test);
+	ft_printf("%s\n", "Writing output program to ");
 	write(fd, am, sizeof(am->hd.magic) + sizeof(am->hd.prog_name)
 			+ sizeof(am->hd.prog_size) + sizeof(am->hd.comment));
 	close(fd);
