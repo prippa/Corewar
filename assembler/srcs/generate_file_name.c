1f0cc509b70a873e26905e441f9a133298e8e049
Dude-Rocker <realslavko@gmail.com>
2018-05-27 18:28:49 +0300

    All good (mb norm)

--- assembler/srcs/generate_file_name.c
+++ assembler/srcs/generate_file_name.c
@@ -6,56 +6,23 @@
 /*   By: vgladush <vgladush@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 17:33:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/05/26 14:55:31 by vgladush         ###   ########.fr       */
+/*   Updated: 2018/05/27 18:27:17 by vgladush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static int		ft_string_size(char *str)
-{
-	int i;
-	int len;
-
-	i = 0;
-	len = ft_strlen(str) - 1;
-	while (len > -1 && str[len] != '/')
-	{
-		i++;
-		len--;
-	}
-	return (i);
-}
-
-static int		len_to_copy(char *str)
-{
-	int i;
-	int len;
-
-	i = 0;
-	len = ft_strlen(str) - 1;
-	while (str[len] && str[len] != '.' && str[len] != '/')
-	{
-		i++;
-		len--;
-	}
-	if (ft_string_size(str) == i)
-		return (0);
-	return (i + 1);
-}
-
 char			*generate_file_name(char *file_string)
 {
 	int		len;
-	int		copy_start;
-	char	*tmp;
 	char	*res;
 
-	len = ft_string_size(file_string);
-	copy_start = ft_strlen(file_string) - len;
-	tmp = ft_strnew(len - len_to_copy(file_string));
-	ft_strncpy(tmp, &file_string[copy_start], len - len_to_copy(file_string));
-	res = ft_strjoin(tmp, ".cor");
-	free(tmp);
+	len = ft_strlen(file_string) - 1;
+	while (len > 0 && file_string[len] != '/' && file_string[len] != '.')
+		len--;
+	if (!len || file_string[len] == '/')
+		len = ft_strlen(file_string);
+	file_string[len] = 0;
+	res = ft_strjoin(file_string, ".cor");
 	return (res);
 }
--- assembler/srcs/write_command2.c
+++ assembler/srcs/write_command2.c
@@ -6,7 +6,7 @@
 /*   By: vgladush <vgladush@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 11:43:00 by vgladush          #+#    #+#             */
-/*   Updated: 2018/05/27 00:09:57 by vgladush         ###   ########.fr       */
+/*   Updated: 2018/05/27 18:12:50 by vgladush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ void	write_cod5(char *s, t_asm *am, char cmd, t_command *tm)
 	tm->codage = (T_DTR(s[am->x]) ? 0xd4 : 0x54);
 	tm->codage = (s[am->x] == DIRECT_CHAR ? 0x94 : tm->codage);
 	tm->arg1.tp = (s[am->x] == 'r' ? T_REG : T_DIR);
+	tm->arg1.tp = (s[am->x] == DIRECT_CHAR ? T_IND : tm->arg1.tp);
 	am->x += (s[am->x] == 'r' || s[am->x] == DIRECT_CHAR ? 1 : 0);
 	crt_arg(s, am, &tm->arg1, cmd);
 	bef_error(s, am, cmd, 2);
@@ -76,6 +77,7 @@ void	write_cod5(char *s, t_asm *am, char cmd, t_command *tm)
 	tm->codage += (T_DTR(s[am->x]) ? 0x20 : 0);
 	tm->codage += (s[am->x] == DIRECT_CHAR ? 0x10 : 0);
 	tm->arg2.tp = (s[am->x] == 'r' ? T_REG : T_DIR);
+	tm->arg2.tp = (s[am->x] == DIRECT_CHAR ? T_IND : tm->arg2.tp);
 	am->x += (s[am->x] == 'r' || s[am->x] == DIRECT_CHAR ? 1 : 0);
 	crt_arg(s, am, &tm->arg2, cmd);
 	bef_error(s, am, cmd, 2);
