5809d88529e2f4ad23380d0fb8ad91dbdd6380cd
contrbas@gmail.com <K4rmU78B>
2018-05-28 22:24:30 +0300

    ALL

--- vm/vm_champs_42/champs/examples/bigzork.s
+++ vm/vm_champs_42/champs/examples/bigzork.s
@@ -2,9 +2,6 @@
 .comment "I know i cant win"
 
 ld %0, r2
-
-st r1, 21142
-
 st r1, 211
 st r1, 211
 st r1, 211
@@ -45,19 +42,9 @@ st r1, 211
 st r1, 211
 st r1, 211
 st r1, 211
-loop: 
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %1
-live %23
+st r1, 211
+st r1, 211
+loop:
 live %1
 live %1
 live %1
@@ -78,8 +65,20 @@ live %1
 live %1
 live %1
 live %1
-live %112341234
-live %1,3
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
 live %1
 live %1
 live %1
