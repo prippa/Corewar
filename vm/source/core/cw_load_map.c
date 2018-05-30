5fef0cf83b7609259ff5d22e742741f22f71f9e0
contrbas@gmail.com <K4rmU78B>
2018-05-30 17:22:54 +0300

    ALL

--- vm/source/core/cw_load_map.c
+++ vm/source/core/cw_load_map.c
@@ -13,25 +13,52 @@ int		champ_quantity(t_champ *champ)
 	return (i);
 }
 
-void	init_map(unsigned char *dst, unsigned char *src /*int start*/, unsigned int bot_size, t_champ *var) // for the several bots;
+void	fill_map_with_bots(unsigned char *dst, t_champ *champ)
+{
+	// int champs_quantity;
+	int map_distance;
+	// int total_map_size;
+	unsigned int i;
+	unsigned int j;
+	
+	// champs_quantity = champ_quantity(champ);
+	map_distance = MEM_SIZE / champ_quantity(champ);
+	ft_printf("%d\n", map_distance);
+	// total_map_size = MEM_SIZE;
+	// while (total_map_size)
+	// {
+	// 	while (i < bot_size)
+	// 	{
+	// 		dst[i] = src[i];
+	// 		i++;
+	// 	}
+	// 	i += map_distance;
+	// 	total_map_size -= map_distance;
+	// }
+	i = 0;
+	// j = 0;
+	while (champ)
+	{
+		j = 0;
+		ft_printf("i -> %u\n", i);
+		while (j < champ->head.prog_size)
+		{
+			dst[i] = champ->code[j];
+			i++;
+			j++;
+		}
+		i += map_distance - j;
+		champ = champ->next;
+	}
+}
+
+void	init_map(unsigned char *dst)
 {
 	unsigned int i;
-	int map_distance;
 
 	i  = 0;
-	map_distance = MEM_SIZE / champ_quantity(var);
-	ft_printf("%d\n", map_distance);
-
 	while (i < MEM_SIZE)
 		dst[i++] = '0';
-
-	// first player;
-	i = 0;
-	while (i < bot_size)
-	{
-		dst[i] = src[i];
-		i++;
-	}
 }	
 
 void	display_map(unsigned char *map)
@@ -46,7 +73,7 @@ void	display_map(unsigned char *map)
 	while (i < MEM_SIZE)
 	{
 		if (map[i] != '0')
-			ft_printf("%~.2x", F_GREEN, map[i]);
+			ft_printf("%~.2x", F_GREEN, map[i]); // adopt + 1;
 		else
 		{
 			ft_printf("%~c", F_WHITE, map[i]);
@@ -66,6 +93,8 @@ void	display_map(unsigned char *map)
 
 void	cw_load_map()
 {
-	init_map(g_cw->map.stack, g_cw->pd.champs->code, g_cw->pd.champs->head.prog_size, g_cw->pd.champs);
+	init_map(g_cw->map.stack/*, g_cw->pd.champs->code, g_cw->pd.champs->head.prog_size, g_cw->pd.champs*/);
+
+	fill_map_with_bots(g_cw->map.stack, g_cw->pd.champs);
 	display_map(g_cw->map.stack);
 }
