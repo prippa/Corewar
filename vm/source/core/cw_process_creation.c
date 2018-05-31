08939ac635bc2337f7e52951f6cfc0eb45509b3e
contrbas@gmail.com <K4rmU78B>
2018-05-31 20:25:46 +0300

    ALL

--- vm/source/core/cw_process_creation.c
+++ vm/source/core/cw_process_creation.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cw_process_creation.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otimofie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/31 20:25:40 by otimofie          #+#    #+#             */
+/*   Updated: 2018/05/31 20:25:41 by otimofie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "corewar.h"
 
-t_processes	*cw_process_node_constructor(unsigned char *code, int color, int process_PC)
+t_processes	*cw_constr(unsigned char *code, int color, int process_pc)
 {
-	int i;
+	int			i;
 	t_processes *new;
 
 	i = 0;
-	if(!(new = (t_processes *)malloc(sizeof (t_processes))))
+	if (!(new = (t_processes *)malloc(sizeof(t_processes))))
 		return (NULL);
 	while (i < CHAMP_MAX_SIZE + 1)
 	{
@@ -14,38 +26,38 @@ t_processes	*cw_process_node_constructor(unsigned char *code, int color, int pro
 		i++;
 	}
 	new->color = color;
-	new->process_PC = process_PC;
+	new->process_PC = process_pc;
 	new->carry = 0;
 	ft_bzero(new->registers, 16);
 	new->next = NULL;
 	return (new);
 }
 
-void	cw_init_processes(t_champ *champ_pointer, t_processes **processes_pointer)
+void	cw_init_processes(t_champ *champ_pointer, t_processes **proc_p)
 {
-	int color;
-	int process_PC;
-	int map_distance;
-	t_processes *tmp_processes;
-	
+	int			color;
+	int			process_pc;
+	int			map_distance;
+	t_processes *tmp;
+
 	color = 4;
 	map_distance = MEM_SIZE / g_cw->pd.champs_count;
 	process_PC = MEM_SIZE - (MEM_SIZE / g_cw->pd.champs_count);
 	if (g_cw->pd.champs_count == 3)
-		process_PC -= 1;
-	while(champ_pointer)
+		process_pc -= 1;
+	while (champ_pointer)
 	{
-		if (!*processes_pointer)
-			*processes_pointer = cw_process_node_constructor(champ_pointer->code, color, process_PC);
+		if (!*proc_p)
+			*proc_p = cw_constr(champ_pointer->code, color, process_pc);
 		else
 		{
-			tmp_processes = *processes_pointer;
-			while (tmp_processes->next)
-				tmp_processes = tmp_processes->next;
-			tmp_processes->next = cw_process_node_constructor(champ_pointer->code, color, process_PC);
+			tmp = *proc_p;
+			while (tmp->next)
+				tmp = tmp->next;
+			tmp->next = cw_constr(champ_pointer->code, color, process_pc);
 		}
 		color--;
-		process_PC -= map_distance;
+		process_pc -= map_distance;
 		champ_pointer = champ_pointer->next;
 	}
 }
