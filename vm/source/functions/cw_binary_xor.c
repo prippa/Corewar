cb973f09f7b7016822ed46e652f1e1132dd39737
contrbas@gmail.com <K4rmU78B>
2018-06-01 14:53:15 +0300

    ALL

--- vm/source/functions/cw_binary_xor.c
+++ vm/source/functions/cw_binary_xor.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "asm.h"
+#include "corewar.h"
 
 static int		ft_pow(int nb, int pow)
 {
