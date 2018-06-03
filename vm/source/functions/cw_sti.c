e4943e013d8170ad55087b0c0a286ddbff930305
contrbas@gmail.com <K4rmU78B>
2018-06-03 16:35:41 +0300

    ALL

--- vm/source/functions/cw_sti.c
+++ vm/source/functions/cw_sti.c
@@ -120,5 +120,7 @@ void	cw_sti(t_command *cmd, t_stack *map, t_processes *process) // do not forget
 	}
 	map->stack[process->process_PC] = 1;
 	map->stack_color[process->process_PC] = 2;
-	cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
+	// cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
+
+	ft_printf("%d\n", 15 & IDX_MOD);
 }
