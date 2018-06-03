febeefa340d75c959cb5531595e5f82e29ced888
contrbas@gmail.com <K4rmU78B>
2018-06-03 21:37:27 +0300

    ALL

--- vm/source/functions/cw_sti.c
+++ vm/source/functions/cw_sti.c
@@ -169,7 +169,8 @@ void	cw_sti(t_command *cmd, t_stack *map, t_processes *process) // do not forget
 	// maybe all colors have to have champions name id;
 	// and the champions name is the main parent for all stuff;
 	// maybe process id == champions name ?
-
+	// fot the process_PC management;
+	
 	if (process->registers[0] == -1) // testing; // modify color function according tothe champs num
 		map->stack_color[process->process_PC] = 5;
 
