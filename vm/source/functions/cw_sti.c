cc138e47199283da305e31f313d8f9f7bd1286f2
contrbas@gmail.com <K4rmU78B>
2018-06-03 14:11:04 +0300

    ALL

--- vm/source/functions/cw_sti.c
+++ vm/source/functions/cw_sti.c
@@ -15,7 +15,7 @@ int		cw_pow(int base, int level)
 	return (res);
 }
 
-int		cw_bin_to_in(char *str) // process id in order to find the correct process;
+int		cw_bin_to_in(char *str, int index) // process id in order to find the correct process;
 {
 	int decimal;
 	int	len;
@@ -26,36 +26,78 @@ int		cw_bin_to_in(char *str) // process id in order to find the correct process;
 	i = 0;
 	while (len >= 0)
 	{
-		if (str[len] == '1')
+		if (str[index] == '1')
+		{
 			decimal += cw_pow(2, i);
+			ft_printf("true i -> %d\n", i);
+		}
 		i++;
+		index--;
 		len--;
 	}
+	// ft_printf("decimal -> %d", decimal);
 	return (decimal);
 }
 
 void	cw_sti(t_command *cmd, t_stack *map, t_processes *process) // move the process_PC by the quantity of bytes; // process id // process parent;
 {
-	ft_printf("%d\n", cmd->arg2.av);
-	ft_printf("%d\n", cmd->arg3.av);
+	// ft_printf("%d\n", cmd->arg2.av);
+	// ft_printf("%d\n", cmd->arg3.av);
 	
 	process->process_PC += ((cmd->arg2.av + process->registers[cmd->arg3.av]) % IDX_MOD);
 	 // it will be found by the color ?; // process_PC will be moved by the quantity of bits,
 		// not by the place where the information has to be placed;
 
-	ft_printf("process_PC -> %d\n", process->process_PC);
+	// ft_printf("process_PC -> %d\n", process->process_PC);
 
-	char *str = ft_itoa_base(-1, 2, 87);
+	char *str = ft_itoa_base(5, 2, 87);
+
+	ft_printf("itoa_base -> %s\n", str);
+
+
+	int bla = 0;
+
+
+
+	bla = 0;
+
+
+	char *check = ft_strnew(32);
+	bla = 0;
+	while (bla < 32)
+	{
+		check[bla] = '0';
+		bla++;
+	}
+	// ft_bzero(check, 32);
+	ft_strncpy(&check[29], &str[0], 3);
+
+	ft_printf("check -> %s\n", check);
 
 	unsigned char buf[4];
 	int a = 0;
+	int k;
+
 	while (a < 4)
 	{
-		buf[a] = cw_bin_to_in(&str[a + 8]);
+		if (a == 0)
+		{
+			k = 7 ;
+			buf[a] = cw_bin_to_in(check, k);
+		}
+		else if (a >= 0)
+		{
+			k = k + 8;
+			ft_printf("k->%d", k);
+			buf[a] = cw_bin_to_in(check, k);
+		}
+		// buf[a] =1;
+		ft_printf(" to_dec -> %d\n", buf[a]);
+		
 		a++;
 	}
 	
-	ft_printf("process_PC -> %s\n", str);
+	// ft_printf("process_PC -> %s\n", str);
 	
 	int i = 0;
 	int	j = process->process_PC;
@@ -65,5 +107,8 @@ void	cw_sti(t_command *cmd, t_stack *map, t_processes *process) // move the proc
 		map->stack[j] = buf[i++];
 		map->stack_color[j++] = process->color;
 	}
-	cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
+
+	free(str);
+	free(check);
+	// cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
 }
