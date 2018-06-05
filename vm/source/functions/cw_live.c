644e2a7cd0748e70cd731b8c8a489bb7a30a8e53
contrbas@gmail.com <K4rmU78B>
2018-06-05 19:45:03 +0300

    - added the Goals file;

--- vm/source/functions/cw_live.c
+++ vm/source/functions/cw_live.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 19:11:31 by otimofie          #+#    #+#             */
-/*   Updated: 2018/06/05 19:17:36 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/06/05 19:36:57 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 //1. Засчитывает, что процесс (который выполняет данную команду) жив.
 //2. Засчитывает, что жив номер (если этот номер совпадает с номером игрока, то засчитвает, что этот игрок жив), который заходит как аргумент (T_DIR).
 
-void			cw_live(t_command *cmd, t_stack *map, unsigned int process_id) // TODO think over globak champ;
+void			cw_live(t_command *cmd, t_stack *map, unsigned int process_id) // TODO think over global champ;
 {
     t_processes *proc;
 
