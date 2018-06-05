f0667dfe6a3ed55667b324744ddc8ce835c1ec72
contrbas@gmail.com <K4rmU78B>
2018-06-05 09:13:57 +0300

    ALL

--- vm/source/functions/cw_sti.c
+++ vm/source/functions/cw_sti.c
@@ -192,8 +192,8 @@ void	cw_sti(t_command *cmd, t_stack *map, t_processes *process, int process_id)
 	// each process has the color of the parent;
 
 		// for testing;
-	map->stack[proc->process_PC] = 7;
-	map->stack_color[proc->process_PC] = 5;
+	// map->stack[proc->process_PC] = 7;
+	// map->stack_color[proc->process_PC] = 5;
 
 
 
