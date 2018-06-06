4eb992156ae9c7a0a6c55b7e56c200a6c2454ae8
contrbas@gmail.com <K4rmU78B>
2018-06-06 11:48:23 +0300

    ALL

--- vm/source/functions/cw_live.c
+++ vm/source/functions/cw_live.c
@@ -15,6 +15,7 @@
 // Выполняет 2 операции:
 //1. Засчитывает, что процесс (который выполняет данную команду) жив.
 //2. Засчитывает, что жив номер (если этот номер совпадает с номером игрока, то засчитвает, что этот игрок жив), который заходит как аргумент (T_DIR).
+// we take the next live process in the linked list of processes;
 
 void			cw_live(t_command *cmd, t_stack *map, t_processes *proc/*, unsigned int process_id*/) // TODO think over global champ;
 {
