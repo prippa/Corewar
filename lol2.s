1b1072f6d5969c044a508ff0657102f7e5081b28
prippa <pavelrippa@gmail.com>
2018-06-06 12:17:03 +0300

    update

--- lol2.s
+++ lol2.s
@@ -1,4 +1,4 @@
 .name		"test1"
 .comment	"simple fork"
 
-sti r1,r1,r1
+ld 2, r3
--- vm/source/functions/cw_ld.c
+++ vm/source/functions/cw_ld.c
@@ -13,10 +13,14 @@ void			cw_ld(t_command *cmd, t_stack *map, t_processes *proc/*, unsigned int pro
 		proc->registers[cmd->arg2.av - 1] =
 		cw_get_dec_from_the_point(map->stack,
 		4,
-		proc->process_PC + cmd->arg1.av); // TODO check out -> process_PC;
+		(proc->process_PC + cmd->arg1.av) % MEM_SIZE); // TODO check out -> process_PC;
+		proc->process_PC += cmd->arg1.tp + cmd->arg2.tp + 2;
 	}
 	proc->carry = (proc->registers[cmd->arg2.av - 1] == 0 ? 1 : 0); // carry
-//	ft_printf("carry = %d\n", proc->carry);
-//	for(int i = 0; i < REG_NUMBER; i++)
-//		ft_printf("%d ", proc->registers[i]);
+	 map->stack[proc->process_PC] = 7;
+	 map->stack_color[proc->process_PC] = 5;
+
+	 ft_printf("process_carry -> %d\n\n", proc->carry);
+
+	 cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
 }
