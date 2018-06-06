70ea0f519b41979180a8961291eabc87091710d3
contrbas@gmail.com <K4rmU78B>
2018-06-06 17:24:00 +0300

    ALL

--- vm/source/functions/cw_zjmp.c
+++ vm/source/functions/cw_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 16:17:53 by otimofie          #+#    #+#             */
-/*   Updated: 2018/06/06 17:19:38 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/06/06 17:23:34 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void			cw_zjmp(t_command *cmd, t_stack *map, t_processes *proc/*, unsigned int p
     if (proc->carry == 1)
         proc->process_PC = MEM_CORRECTION(proc->process_PC + IDX_CORRECTION(cmd->arg1.av));
     else
-        proc->process_PC = MEM_CORRECTION(proc->process_PC + cmd->arg1.av);
+        proc->process_PC = MEM_CORRECTION(proc->process_PC + cmd->arg1.tp + 1);
 
 //
-//    map->stack[proc->process_PC] = 7;
-//    map->stack_color[proc->process_PC] = 5;
-//
-//    cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
+    map->stack[proc->process_PC] = 7;
+    map->stack_color[proc->process_PC] = 5;
+
+    cw_display_map(g_cw->map.stack, g_cw->map.stack_color);
 }
