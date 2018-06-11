110bc82e8752bd997b030c62a52ccd23b1eefbc3
prippa <pavelrippa@gmail.com>
2018-06-11 14:38:35 +0300

    update

--- vm/source/main.c
+++ vm/source/main.c
@@ -12,30 +12,30 @@
 
 #include "corewar.h"
 
-void print_one_proc(t_processes *proc)
-{
-	if (!proc)
-	{
-		ft_putstr("proc == NULL\n");
-		return ;
-	}
-	ft_printf("id - %d\n", proc->id);
-	ft_printf("color - %d\n", proc->color);
-	ft_printf("process_PC - %d\n", proc->process_PC);
-	ft_printf("carry - %d\n", proc->carry);
-}
+// void print_one_proc(t_processes *proc)
+// {
+// 	if (!proc)
+// 	{
+// 		ft_putstr("proc == NULL\n");
+// 		return ;
+// 	}
+// 	ft_printf("id - %d\n", proc->id);
+// 	ft_printf("color - %d\n", proc->color);
+// 	ft_printf("process_PC - %d\n", proc->process_PC);
+// 	ft_printf("carry - %d\n", proc->carry);
+// }
 
-void print_processes(t_processes *start)
-{
-	int i = 1;
-	while (start)
-	{
-		ft_printf("PROCESSE ------------№%d-----------\n", i);
-		print_one_proc(start);
-		start = start->next;
-		i++;
-	}
-}
+// void print_processes(t_processes *start)
+// {
+// 	int i = 1;
+// 	while (start)
+// 	{
+// 		ft_printf("PROCESSE ------------№%d-----------\n", i);
+// 		print_one_proc(start);
+// 		start = start->next;
+// 		i++;
+// 	}
+// }
 
 int		main(int argc, char **argv)
 {
