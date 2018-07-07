661aefb483458a48a7ecff5f25e31658f00e675c
contrbas@gmail.com <K4rmU78B>
2018-07-07 12:22:18 +0300

    ALL

--- vm/source/functions/cw_live.c
+++ vm/source/functions/cw_live.c
@@ -74,7 +74,7 @@ void			cw_live(t_command *cmd, t_stack *map, t_processes *proc/*, unsigned int p
 	if (map->stack[proc->process_PC] != 0 || map->stack_color[proc->process_PC] != 0) 
 	{
 										// if (proc->id == 23)
-		ft_printf("%~s\n", F_BACK_RED_WHITE, "not valid cmd 1");
+		// ft_printf("%~s\n", F_BACK_RED_WHITE, "not valid cmd 1");
 		map->stack_color[proc->process_PC] = proc->proc_process_PC_color; // move the color of the PC;
 	}
 	else if(map->stack[proc->process_PC] == 0 && map->stack_color[proc->process_PC] == 0)
