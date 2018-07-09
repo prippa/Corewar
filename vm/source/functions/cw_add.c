b40e3c1e466dd20bd2a800456bf0a3f372a5d6d5
prippa <pavelrippa@gmail.com>
2018-07-10 00:57:38 +0300

    add sub

--- vm/source/functions/cw_add.c
+++ vm/source/functions/cw_add.c
@@ -12,7 +12,29 @@
 
 #include "corewar.h"
 
+static void		cw_execute_add(t_command *cmd, t_processes *proc)
+{
+	proc->registers[cmd->arg3.av - 1] =
+		proc->registers[cmd->arg1.av - 1] + proc->registers[cmd->arg2.av - 1];
+	proc->carry = (proc->registers[cmd->arg3.av - 1] == 0) ? 1 : 0;
+}
+
+static int		cw_get_args_av_add(t_command *cmd, t_processes *proc)
+{
+	if (cmd->codage == RRR)
+		cw_set_arg_av(cmd, proc->pc + CODAGE_SKIP);
+	else
+		return (0);
+	return (1);
+}
+
 void			cw_add(t_processes *proc)
 {
-	
+	t_command cmd;
+
+	ft_bzero(&cmd, sizeof(t_command));
+	cw_get_codage_and_arg_tp(&cmd, proc);
+	if (cw_get_args_av_add(&cmd, proc) && cw_is_valid_reg(&cmd))
+		cw_execute_add(&cmd, proc);
+	cw_move_pc_with_codage(&cmd, proc);
 }
