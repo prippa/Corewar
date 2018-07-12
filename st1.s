ed83e0ab1c7cdf7de6c61bc963d65bae600d1fb1
contrbas@gmail.com <K4rmU78B>
2018-07-12 18:27:47 +0300

    ALL

--- st1.s
+++ st1.s
@@ -6,9 +6,10 @@
 # ld		%33554431, r15
 # ld		%-1, r16
 
-
-# ld		%7, r10
-# ld		%7, r11
+# added to the zero value;
+ld		%190055938, r7
+ld		%4, r10
+ld		%0, r11
 # ld		%7, r12
 # ld		%7, r13
 
@@ -25,11 +26,28 @@
 
 # add data	adopt fork;
 
+# +- 10 for changing
+# 1,2 + 1 = 2,3
+# add only the first one;
+# to the different points and then get the simultaneous;
+# 2,1, 3,4 fork after -> fork jump
+
+# 8 <-> sti + jump i need live;
+
+#0,1 -> 1,2
+
+# forward 2
 
 live	%-1
-fork	%3
-zjmp	%1
-fork	%3
+fork	%8
+add		r11, r10, r11
+zjmp	%2
+live	%-1
+live	%-1
+live	%-1
+sti     r7, r11, %100
+# synchronize
+# fork	%3
 
 
 
