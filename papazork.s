572586e1f80a208052f6dbd52e283b2d8c888ac0
prippa <pavelrippa@gmail.com>
2018-07-15 20:35:45 +0300

    papazork

---
+++ papazork.s
@@ -0,0 +1,204 @@
+.name "papazork"
+.comment "PAPA is coming for YOU!"
+
+ld %0, r2
+st r1, 6
+live %1
+fork %-17
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+st r1, 6
+live %1
+
+zjmp %511
